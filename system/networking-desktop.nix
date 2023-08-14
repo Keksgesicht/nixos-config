04d1bd050e48018d90d4756bfc8b6faf1bdd99aa
Jan B <git@keksgesicht.net>
2023-08-14 10:05:33 +0200

    fix permission of NetworkManager connection files
    failed to load connection: File permissions (100444) are insecure

--- system/networking-desktop.nix
+++ system/networking-desktop.nix
@@ -16,34 +16,41 @@
     # network connections on all systems
     "NetworkManager/system-connections/TU_Darmstadt.nmconnection" = {
       source = ../secrets/linux-root/etc/NetworkManager/system-connections/TU_Darmstadt.nmconnection;
+      mode = "0600";
     };
 
     # network connections on server/desktop
     "NetworkManager/system-connections/dmz.nmconnection" = {
       enable = (config.networking.hostName == "cookieclicker");
       source = ../secrets/linux-root/etc/NetworkManager/system-connections/dmz.nmconnection;
+      mode = "0600";
     };
     "NetworkManager/system-connections/home.nmconnection" = {
       enable = (config.networking.hostName == "cookieclicker");
       source = ../secrets/linux-root/etc/NetworkManager/system-connections/home.nmconnection;
+      mode = "0600";
     };
     "NetworkManager/system-connections/wg-server.nmconnection" = {
       enable = (config.networking.hostName == "cookieclicker");
       source = ../secrets/linux-root/etc/NetworkManager/system-connections/wg-server.nmconnection;
+      mode = "0600";
     };
 
     # network connections on laptop
     "NetworkManager/system-connections/eduroam.nmconnection" = {
       enable = (config.networking.hostName == "cookiethinker");
       source = ../secrets/linux-root/etc/NetworkManager/system-connections/eduroam.nmconnection;
+      mode = "0600";
     };
     "NetworkManager/system-connections/nach_Hause_telefonieren.nmconnection" = {
       enable = (config.networking.hostName == "cookiethinker");
       source = ../secrets/linux-root/etc/NetworkManager/system-connections/nach_Hause_telefonieren.nmconnection;
+      mode = "0600";
     };
     "NetworkManager/system-connections/wlan00.nmconnection" = {
       enable = (config.networking.hostName == "cookiethinker");
       source = ../secrets/linux-root/etc/NetworkManager/system-connections/wlan00.nmconnection;
+      mode = "0600";
     };
   };
 
