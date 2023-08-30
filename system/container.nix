dc2a30a5955e80fccc34cf71e4fabdc23e24b57e
Jan B <git@keksgesicht.net>
2023-08-30 10:40:55 +0200

    weekly cleanup of unused container images

--- system/container.nix
+++ system/container.nix
@@ -13,6 +13,12 @@
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
+
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+        flags = [ "--all" ];
+      };
     };
 
     containers = {
