ed2380cf5ca92a8dea91c7b04f4866a6a7d5e4d3
Jan B <git@keksgesicht.net>
2023-08-30 11:12:48 +0200

    use home-manager channel

--- desktop/home-manager.nix
+++ desktop/home-manager.nix
@@ -1,13 +1,11 @@
 { config, pkgs, ...}:
 
-let
-  # $AUTH nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz";
-in
 {
   imports = [
-    #<home-manager/nixos>
-    (import "${home-manager}/nixos")
+    # $AUTH nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+    # $AUTH nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+    # $AUTH nix-channel --update
+    <home-manager/nixos>
   ];
   # The home.stateVersion option does not have a default and must be set
   home-manager.users.keks.home.stateVersion = "18.09";
