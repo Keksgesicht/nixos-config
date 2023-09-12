0141ae75b11c9f9532d3db6554d97a3f9cf079be
Jan B <git@keksgesicht.net>
2023-09-13 00:01:51 +0200

    fix possible event creation bomb by audio setup script

--- files/packages/my-audio/lib/audio-auto-relink.sh
+++ files/packages/my-audio/lib/audio-auto-relink.sh
@@ -2,14 +2,14 @@
 
 case $1 in
 	sink)
-		pactl subscribe | rg --line-buffered "Event 'new' on sink" | \
+		pactl subscribe | rg --line-buffered "Event 'new' on sink #" | \
 		while true; do
 			read
 			${work_dir}/audio-setup-hw-sink.sh
 		done
 	;;
 	source)
-		pactl subscribe | rg --line-buffered "Event 'new' on source" | \
+		pactl subscribe | rg --line-buffered "Event 'new' on source #" | \
 		while true; do
 			read
 			${work_dir}/audio-setup-hw-source.sh
