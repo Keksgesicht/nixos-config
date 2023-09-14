f737c4937ffb6be9c80b34ea55d63f12625f9e97
Jan B <git@keksgesicht.net>
2023-09-14 14:53:25 +0200

    fix not able to `git commit` in CLI

--- development/desktop.nix
+++ development/desktop.nix
@@ -6,6 +6,7 @@ in
 {
   environment.systemPackages = with pkgs; [
     pandoc
+    pinentry."${gpg-cfg.agent.pinentryFlavor}"
     (ventoy.override {
       withQt5 = (config.services.xserver.enable);
     })
