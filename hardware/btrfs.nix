3c820a6c54a66ff682a01d610f432ce8bc93ef4c
Jan B <git@keksgesicht.net>
2023-10-16 00:59:17 +0200

    BTRFS only every 3 month in the night
    or with the first PC start in the morning

--- hardware/btrfs.nix
+++ hardware/btrfs.nix
@@ -3,7 +3,9 @@
 {
   services.btrfs.autoScrub = {
     enable = true;
-    interval = "monthly";
+    # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/tasks/filesystems/btrfs.nix
+    # the above says the timer is "Persistent".
+    interval = "*-2,5,8,11-13 04:20:42";
     fileSystems =
       if (config.networking.hostName == "cookieclicker") then
         [
