64a0495f71addb7cade3f628e2572db669044d1e
Jan B <git@keksgesicht.net>
2023-10-16 20:33:58 +0200

    start garbage collection more often (every 3 days)

--- nix/garbage-collect.nix
+++ nix/garbage-collect.nix
@@ -4,8 +4,8 @@
   nix.gc = {
     automatic = true;
     persistent = true;
-    dates = "weekly";
-    randomizedDelaySec = "42min";
+    dates = "*-*-3,6,9,12,15,18,21,24,27,30 01:23:45";
+    randomizedDelaySec = "23min";
     options = "--delete-older-than 23d";
   };
 }
