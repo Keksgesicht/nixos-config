1917ae7eca98b989eda96400e949edccf4803111
Jan B <git@keksgesicht.net>
2023-11-06 23:05:22 +0100

    tools to list memory information

--- hardware/default.nix
+++ hardware/default.nix
@@ -7,6 +7,7 @@
 
   environment.systemPackages = with pkgs; [
     compsize
+    dmidecode
     efibootmgr
     e2fsprogs
     gptfdisk
@@ -15,6 +16,16 @@
     })
     iftop
     iotop
+    i2c-tools # https://superuser.com/questions/519822/how-to-check-ram-timings-in-linux#answer-1499521
+    /*
+     * sudo modprobe eeprom
+     * sudo modprobe at24
+     * sudo modprobe i2c-i801
+     * sudo modprobe i2c-amd-mp2-pci
+     * sudo modprobe ee1004
+     * decode-dimms
+     */
+    lshw
     parted
     pciutils
     usbutils
