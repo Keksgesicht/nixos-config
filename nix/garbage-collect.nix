db938e0c924e92d5dfcedda04eb9b264d3c46e49
Jan B <git@keksgesicht.net>
2023-11-15 15:58:09 +0100

    adjust garbage collection

--- nix/garbage-collect.nix
+++ nix/garbage-collect.nix
@@ -4,8 +4,8 @@
   nix.gc = {
     automatic = true;
     persistent = true;
-    dates = "*-*-3,6,9,12,15,18,21,24,27,30 01:23:45";
-    randomizedDelaySec = "23min";
-    options = "--delete-older-than 23d";
+    dates = "*-*-5,10,15,20,25,30 01:23:45";
+    randomizedDelaySec = "30min";
+    options = "--delete-older-than 20d";
   };
 }
