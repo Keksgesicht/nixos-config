15be975f14b93acafc21f731841d065c3143aef7
Jan B <git@keksgesicht.net>
2023-11-23 14:15:43 +0100

    home-manager default packages and pkgs instance

--- desktop/home-manager.nix
+++ desktop/home-manager.nix
@@ -9,4 +9,13 @@
   ];
   # The home.stateVersion option does not have a default and must be set
   home-manager.users."keks".home.stateVersion = "23.05";
+
+  # By default, Home Manager uses a private pkgs instance [...]
+  # To instead use the global pkgs that is configured via the system level nixpkgs options,
+  # set the following to `true`
+  home-manager.useGlobalPkgs = true;
+
+  # By default packages will be installed to $HOME/.nix-profile
+  # but they can be installed to /etc/profiles if following is set to `true`
+  home-manager.useUserPackages = true;
 }
