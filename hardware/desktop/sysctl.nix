572393397de417fc71760b9d84b00838e6d0bf0c
Jan B <git@keksgesicht.net>
2023-12-27 13:22:59 +0100

    add information to sysctl config

--- hardware/desktop/sysctl.nix
+++ hardware/desktop/sysctl.nix
@@ -1,4 +1,7 @@
-{ config, pkgs, ... }:
+# https://wiki.archlinux.org/title/sysctl
+# https://man7.org/linux/man-pages/man5/proc.5.html
+
+{ config, ... }:
 
 {
   boot.kernel.sysctl = {
@@ -6,6 +9,7 @@
     "kernel.panic" = 42;
 
     # improve compilation
+    # https://man7.org/linux/man-pages/man7/inotify.7.html
     "fs.inotify.max_user_instances" = 1024;
     "fs.inotify.max_user_watches" = 1048576;
 
@@ -13,6 +17,13 @@
     # Fedora 39: https://fedoraproject.org/wiki/Changes/IncreaseVmMaxMapCount
     # Brodie Robertson: https://www.youtube.com/watch?v=PsHRbfZhgXM
     "vm.max_map_count" = 2147483642;
+
+    # enable ptrace attach mode for normal processes
+    # echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+    # NOT recommended
+    # use `sudo` when using `-p` with `strace`
+    # https://man7.org/linux/man-pages/man2/ptrace.2.html
+    #"sys.kernel.yama.ptrace_scope" = 1;
 
     # enable router advertisement on LAN network adapter
     # combined with /etc/NetworkManager/dispatcher.d/50-public-ipv6
