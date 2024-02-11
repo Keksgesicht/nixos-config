a19a2866f889347676c9a9d6e7196d8be0994a44
Jan B <git@keksgesicht.net>
2024-02-11 15:21:16 +0100

    solution for running out of ram space when building larger projects

--- nix/basic.nix
+++ nix/basic.nix
@@ -42,4 +42,15 @@ with my-functions;
     # enable flake commands
     settings.experimental-features = [ "nix-command" "flakes" ];
   };
+
+  # running out of space when building larger projects
+  # e.g. chromium for robotnix
+  # run `nix build` with `--keep-failed` and do this:
+  #   $AUTH mkdir -p /var/tmp/nix-daemon
+  #   $AUTH rsync -avHAX --remove-source-files /tmp/nix-daemon/ /var/tmp/nix-daemon/
+  #   $AUTH mount --bind /var/tmp/nix-daemon /tmp/nix-daemon
+  systemd = {
+    services.nix-daemon.environment.TMPDIR = "/tmp/nix-daemon";
+    tmpfiles.rules = [ "q /tmp/nix-daemon 1777 root root 10d" ];
+  };
 }
