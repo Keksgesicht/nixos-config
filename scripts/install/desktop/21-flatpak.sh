d6f1314c3ee0014bcec019e4888cde3f321714e9
Jan B <git@keksgesicht.net>
2024-03-18 00:39:36 +0000

    remove apps converted to nixpak from flatpak stuff

--- scripts/install/desktop/21-flatpak.sh
+++ scripts/install/desktop/21-flatpak.sh
@@ -6,29 +6,17 @@ flatpak remote-add --if-not-exists \
 
 # essential apps
 flatpak install -y 'flathub' \
-	com.github.hluk.copyq \
 	com.github.tchx84.Flatseal
 
 # browser
 flatpak install -y 'flathub' \
-	com.brave.Browser \
-	com.github.Eloston.UngoogledChromium \
 	com.github.micahflee.torbrowser-launcher \
-	dev.vencord.Vesktop \
-	org.ferdium.Ferdium \
-	org.signal.Signal
 
 # office
 flatpak install -y 'flathub' \
-	com.github.jeromerobert.pdfarranger \
-	com.github.xournalpp.xournalpp \
 	com.zettlr.Zettlr \
-	net.jami.Jami \
 	org.kde.kclock \
-	org.kde.okular \
-	org.libreoffice.LibreOffice \
 	org.onlyoffice.desktopeditors \
-	org.videolan.VLC
 
 # hardware
 flatpak install -y 'flathub' \
@@ -41,24 +29,17 @@ flatpak install -y 'flathub' \
 	fr.romainvigier.MetadataCleaner \
 	io.github.seadve.Mousai \
 	org.fedoraproject.MediaWriter \
-	org.gimp.GIMP \
-	org.inkscape.Inkscape \
-	org.kde.kdenlive
 
 # development
 flatpak install -y 'flathub' \
-	com.axosoft.GitKraken \
 	com.visualstudio.code \
 	dev.lapce.lapce \
-	org.kde.okteta \
-	org.texstudio.TeXstudio \
 	pm.mirko.Atoms
 
 # gaming
 flatpak install -y 'flathub' \
 	com.github.Matoking.protontricks \
 	com.heroicgameslauncher.hgl \
-	com.obsproject.Studio \
 	com.valvesoftware.Steam \
 	net.davidotek.pupgui2 \
 	net.lutris.Lutris \
@@ -69,7 +50,3 @@ flatpak install -y 'flathub' \
 	org.freedesktop.Platform.VulkanLayer.MangoHud \
 	org.freedesktop.Platform.VulkanLayer.vkBasalt \
 	org.freedesktop.Platform.VulkanLayer.gamescope
-
-# heavy in size
-flatpak install -y 'flathub' \
-	org.freedesktop.Sdk.Extension.texlive
--- scripts/install/laptop/21-flatpak.sh
+++ scripts/install/laptop/21-flatpak.sh
@@ -6,24 +6,9 @@ flatpak remote-add --if-not-exists \
 
 # essential apps
 flatpak install -y \
-	com.brave.Browser \
-	com.github.Eloston.UngoogledChromium \
-	com.github.hluk.copyq \
-	com.github.tchx84.Flatseal \
-	org.ferdium.Ferdium \
-	org.kde.okular
+	com.github.tchx84.Flatseal
 
 # nice to have
 flatpak install -y \
-	com.axosoft.GitKraken \
-	com.github.jeromerobert.pdfarranger \
-	com.github.xournalpp.xournalpp \
 	org.gnome.Firmware \
-	org.kde.kclock \
-	org.libreoffice.LibreOffice \
-	org.signal.Signal \
-	org.texstudio.TeXstudio \
-	org.videolan.VLC
-
-# heavy in size
-#flatpak install org.freedesktop.Sdk.Extension.texlive
+	org.kde.kclock
