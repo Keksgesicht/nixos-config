07e18b0d4bbe8ae5639100e7a0fe6a7ef5e18870
Jan B <git@keksgesicht.net>
2024-03-20 12:05:04 +0000

    adjust MediaPlayer nixpak usability

--- packages/nixpak/MediaPlayer.nix
+++ packages/nixpak/MediaPlayer.nix
@@ -1,4 +1,4 @@
-{ pkgs, bindHomeDir, ... }:
+{ pkgs, sloth, bindHomeDir, ... }:
 
 let
   name = "MediaPlayer";
@@ -7,7 +7,9 @@ in
   nixpak."${name}" = {
     wrapper = {
       packages = [
-        { package = pkgs.vlc; binName = "vlc"; }
+        { package = pkgs.vlc; binName = "vlc"; appFile = [
+          { args.remove = "%U"; args.extra = "%U"; }
+        ]; }
         pkgs.yt-dlp
       ];
       audio = true;
@@ -19,11 +21,18 @@ in
     };
 
     bubblewrap = {
+      bind.ro = [
+        (sloth.concat' sloth.homeDir "/git")
+        (sloth.concat' sloth.homeDir "/Module")
+        (sloth.xdgDocumentsDir)
+        (sloth.xdgDownloadDir)
+        (sloth.xdgMusicDir)
+        (sloth.xdgVideosDir)
+      ];
       bind.rw = [
         (bindHomeDir name "/.config/vlc")
         (bindHomeDir name "/.local/share/vlc")
       ];
-      sockets.x11 = true; # WTF, during startup needed
     };
   };
 }
