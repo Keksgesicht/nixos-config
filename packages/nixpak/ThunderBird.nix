a5019f253b9299715a7796fa8d2ba6242d44a35a
Jan B <git@keksgesicht.net>
2024-03-28 22:06:12 +0100

    fix ThunderBird not able to open links

--- packages/nixpak/ThunderBird.nix
+++ packages/nixpak/ThunderBird.nix
@@ -1,13 +1,18 @@
-{ pkgs, bindHomeDir, ... }:
+{ pkgs, lib, bindHomeDir, ... }:
 
 let
   name = "ThunderBird";
 in
 {
   nixpak."${name}" = {
-    wrapper.packages = [
-      { package = pkgs.thunderbird; binName = "thunderbird"; }
-    ];
+    wrapper = {
+      packages = [
+        { package = pkgs.thunderbird; binName = "thunderbird"; }
+      ];
+      variables = {
+        XDG_DATA_DIRS = lib.mkForce "";
+      };
+    };
 
     dbus.policies = {
       "org.freedesktop.Notifications" = "talk";
@@ -21,6 +26,7 @@ in
           ("${pkgs.thunderbird}/lib/thunderbird")
           ("/app/etc/thunderbird")
         ]
+        "/sys/bus/pci"
       ];
       bind.rw = [
         (bindHomeDir name "/.thunderbird")
