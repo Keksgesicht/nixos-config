620c4ea6036035217dddf12cd2c2017d756487d3
Jan B <git@keksgesicht.net>
2024-03-30 19:07:17 +0100

    (force) enable wayland for SDDM

--- desktop/kde-plasma.nix
+++ desktop/kde-plasma.nix
@@ -19,14 +19,9 @@ with my-functions;
     displayManager = {
       sddm = {
         enable = true;
+        wayland.enable = true;
         autoNumlock = true;
-        # the line starts with "Current="
-        # https://github.com/NixOS/nixpkgs/blob/nixos-23.05/nixos/modules/services/x11/display-managers/sddm.nix#L39
-        theme = ''
-          breeze
-          CursorTheme=LyraG-cursors
-          Font=Noto Sans,10,-1,50,0,0,0,0,0
-        '';
+        theme = "breeze";
       };
       defaultSession = "plasma";
     };
