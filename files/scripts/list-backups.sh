dce20ecf995038fb0a4f25032b2dce06078d068b
Jan B <git@keksgesicht.net>
2024-04-16 21:25:43 +0200

    extend backup list script with impermance boot copies

--- files/scripts/list-backups.sh
+++ files/scripts/list-backups.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
+bck_root_path="/mnt/main/backup_main/boot/root"
+
 file=$1
 if ! [ -e "${file}" ]; then
 	echo "No such file or directory \"${file}\"" >&2
 	exit 1
 fi
 
-rpath=$(realpath "${file}")
-for rr in $(awk '$4 ~ /bind/ {printf "s|%s|%s|\n", $2, $1}' /etc/fstab); do
-	 rpath=$(echo "${rpath}" | sed -e "${rr}")
+real_path=$(realpath "${file}")
+mount_replacer=$(awk '$4 ~ /bind/ {printf "s|%s|%s|\n", $2, $1}' /etc/fstab)
+for rr in ${mount_replacer}; do
+	 rpath=$(echo "${real_path}" | sed -e "${rr}")
 done
 
 sep_count=$(echo "${rpath}" | awk -F'/' '{print NF}')
 path_d=$(echo "${rpath}" | cut -d '/' -f2)
 
-if [ 4 -le ${sep_count} ] && [ "${path_d}" == "mnt" ]; then
+if [ 4 -le "${sep_count}" ] && [ "${path_d}" == "mnt" ]; then
 	path_r=$(echo "${rpath}" | cut -d '/' -f1-4)
 	path_s=$(echo "${rpath}" | cut -d '/' -f5-)
 else
@@ -42,5 +45,10 @@ else
 	esac
 fi
 
-ls -d1 "${path_r}/.backup/"*"/${path_s}"
 echo "${path_r}/.backup/"'*'"/${path_s}"
+ls -d1 "${path_r}/.backup/"*"/${path_s}"
+
+echo ""
+
+echo "${bck_root_path}/"'*'"${real_path}"
+ls -d1 "${bck_root_path}/"*"${real_path}"
