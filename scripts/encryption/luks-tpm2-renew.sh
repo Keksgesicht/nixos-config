bc015a063244eda910d914c79bacbb0e1f963ced
Jan B <git@keksgesicht.net>
2024-05-08 15:40:44 +0200

    enable the usage of no pin with TPM2 unlock

--- scripts/encryption/luks-tpm2-renew.sh
+++ scripts/encryption/luks-tpm2-renew.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-SCRIPT_DIR=$(dirname $(realpath $0))
-source ${SCRIPT_DIR}/luks-tpm2-setup
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+source "${SCRIPT_DIR}/luks-tpm2-setup"
 
 # reenroll key to TPM 2.0
-systemd-cryptenroll --wipe-slot=tpm2 ${luks_device}
+systemd-cryptenroll --wipe-slot=tpm2 "${luks_device}"
 sleep 1s
 systemd-cryptenroll \
 	--tpm2-device=auto \
-	--tpm2-with-pin=true \
+	--tpm2-with-pin=${TPM_PIN} \
 	--tpm2-pcrs=${tpm_regs} \
-	${luks_device}
+	"${luks_device}"
--- scripts/encryption/luks-tpm2.sh
+++ scripts/encryption/luks-tpm2.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-SCRIPT_DIR=$(dirname $(realpath $0))
-source ${SCRIPT_DIR}/luks-tpm2-setup
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+source "${SCRIPT_DIR}/luks-tpm2-setup"
 
 # enroll key to TPM 2.0
 systemd-cryptenroll \
 	--tpm2-device=auto \
-	--tpm2-with-pin=true \
+	--tpm2-with-pin=${TPM_PIN} \
 	--tpm2-pcrs=${tpm_regs} \
-	${luks_device}
+	"${luks_device}"
