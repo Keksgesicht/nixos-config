a07cf8b3c4cf695e1e1441850d9d4d36f7bd3e05
Jan B <git@keksgesicht.net>
2024-05-13 17:32:18 +0200

    fix binary substituter goes offline

--- nix/build-cache-client.nix
+++ nix/build-cache-client.nix
@@ -16,5 +16,8 @@
       ++ lib.optionals (config.networking.hostName != "cookiepi") [
         "nix-serve.cookiepi.keksgesicht.net:TQ3kzk1z7Pq0aEcY7yA7E2Y6MKju/CVHBk6I/NpCF2E="
       ];
+    # nixos-rebuild failed when a previously online substituters goes offline
+    # this reenables local building
+    fallback = true;
   };
 }
