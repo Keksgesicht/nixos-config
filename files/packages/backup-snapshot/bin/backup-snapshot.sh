34fba56b4b7842035691af8725471075a2b3fe74
Jan B <git@keksgesicht.net>
2024-06-11 17:39:47 +0200

    shellcheck: backup-snapshot and fancontrol-hwmon-fix

--- files/packages/backup-snapshot/bin/backup-snapshot.sh
+++ files/packages/backup-snapshot/bin/backup-snapshot.sh
@@ -1,21 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 today=$(date +%Y-%m-%d)
 new_time=$(date -u +%s -d "$today")
-source $(dirname $0)/btrfs-snapshot
+source "$(dirname "$0")/btrfs-snapshot"
 
-if ls ${dest} | grep -Eq "${today}_[dmw]"; then
+if ls "${dest}" | grep -Eq "${today}_[dmw]"; then
 	echo "backup_${mnt} already done for this day"
 	exit 0
 fi
 
 # backup (readonly btrfs snapshot)
 # backup naming (daily/weekly/monthly)
-last_monthly=$(ls -1 $dest | grep '_m$' | tail -1)
-last_weekly=$(ls -1 $dest | grep '_w$' | tail -1)
-if ( [ -n "$monthly" ] && [ 0 -lt $monthly ] ) && ( [ -z "$last_monthly" ] || [ `time_diff $last_monthly` -ge 30 ] ); then
+last_monthly=$(ls -1 "$dest" | grep '_m$' | tail -1)
+last_weekly=$(ls -1 "$dest" | grep '_w$' | tail -1)
+if { [ -n "$monthly" ] && [ 0 -lt "$monthly" ]; } \
+&& { [ -z "$last_monthly" ] || [ "$(time_diff "$last_monthly")" -ge 30 ]; }; then
 	day="${today}_m"
-elif ( [ -n "$weekly" ] && [ 0 -lt $weekly ] ) && ( [ -z "$last_weekly" ] || [ `time_diff $last_weekly` -ge 7 ] ); then
+elif { [ -n "$weekly" ] && [ 0 -lt "$weekly" ]; } \
+  && { [ -z "$last_weekly" ] || [ "$(time_diff "$last_weekly")" -ge 7 ]; }; then
 	day="${today}_w"
 else
 	day="${today}_d"
@@ -36,19 +38,19 @@ done
 for file in $(ls ${dest} | sort -r); do
 	case $(echo "${file}" | rev | cut -c 1) in
 	"d") # daily
-		daily=$(( ${daily} - 1 ))
+		daily=$(( daily - 1 ))
 		[ ${daily} -lt 0 ] || continue
-		delete ${file}
+		delete "${file}"
 	;;
 	"w") # weekly
-		weekly=$(( ${weekly} - 1 ))
+		weekly=$(( weekly - 1 ))
 		[ ${weekly} -lt 0 ] || continue
-		delete ${file}
+		delete "${file}"
 	;;
 	"m") # monthly
-		monthly=$(( ${monthly} - 1 ))
+		monthly=$(( monthly - 1 ))
 		[ ${monthly} -lt 0 ] || continue
-		delete ${file}
+		delete "${file}"
 	;;
 	esac
 done
--- files/packages/config-fancontrol/bin/fancontrol-hwmon-fix.sh
+++ files/packages/config-fancontrol/bin/fancontrol-hwmon-fix.sh
@@ -1,11 +1,17 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
-file_input="$(dirname $0)/../cfg/fancontrol.config.sample"
+set -e
+
+find-hwmon() {
+	find "$1" -mindepth 1 -maxdepth 1 | tr '/' '\n' | tail -1
+}
+
+file_input="$(dirname "$0")/../cfg/fancontrol.config.sample"
 file_output="/etc/fancontrol"
-cp ${file_input} ${file_output}
+cp "${file_input}" ${file_output}
 
-hwmon_temp1=$(ls /sys/devices/pci0000:00/0000:00:18.3/hwmon | head -n 1)
-hwmon_fan1=$(ls /sys/devices/platform/nct6775.656/hwmon | head -n 1)
+hwmon_temp1=$(find-hwmon /sys/devices/pci0000:00/0000:00:18.3/hwmon)
+hwmon_fan1=$(find-hwmon /sys/devices/platform/nct6775.656/hwmon)
 
-sed -i 's|hwmon_temp1|'${hwmon_temp1}'|g' ${file_output}
-sed -i  's|hwmon_fan1|'${hwmon_fan1}'|g'  ${file_output}
+sed -i 's|hwmon_temp1|'"${hwmon_temp1}"'|g' ${file_output}
+sed -i  's|hwmon_fan1|'"${hwmon_fan1}"'|g'  ${file_output}
