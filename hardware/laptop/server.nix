3b17c31e9288fa8463b4e282d2863d94dca37816
Jan B <git@keksgesicht.net>
2024-06-15 16:51:44 +0200

    disable unused devices on laptop server (bluetooth)

--- hardware/laptop/server.nix
+++ hardware/laptop/server.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # prevent suspend when closing lid
@@ -6,5 +6,19 @@
     lidSwitch = "lock";
     lidSwitchDocked = "lock";
     lidSwitchExternalPower = "lock";
+  };
+
+  systemd.services = {
+    "bluetooth-disable" = {
+      enable = true;
+      wantedBy = [
+        "basic.target"
+      ];
+      after  = [
+        "bluetooth.target"
+        "systemd-rfkill.service"
+      ];
+      script = "${pkgs.util-linux}/bin/rfkill block bluetooth";
+    };
   };
 }
