943e73a096b653e34acb90afa62150677ec76b43
Jan B <git@keksgesicht.net>
2024-10-14 12:17:49 +0200

    avoid login sound (on tower)

--- files/packages/my-audio/bin/audio-init.sh
+++ files/packages/my-audio/bin/audio-init.sh
@@ -4,24 +4,36 @@ work_dir=$(realpath "$(dirname "$0")")
 export work_dir
 source "${work_dir}"/../lib/settings.sh
 
+
+mute_hardware() {
+	### mute all hardware (laptop/mobile)
+	if [ "$(cat /etc/hostname)" = "cookiethinker" ]; then
+		for alsa_dev in $(pactl list sinks short | awk '/alsa/ {print $2}'); do
+			pactl set-sink-mute "${alsa_dev}" 1
+		done
+	fi
+
+	### mute hdmi outputs on desktop
+	if [ "$(cat /etc/hostname)" = "cookieclicker" ]; then
+		for alsa_dev in $(pactl list sinks short | awk '/alsa_output.pci-0000_44_00.1.pro-output/ {print $2}'); do
+			pactl set-sink-mute "${alsa_dev}" 1
+		done
+	fi
+}
+
+
+sleep 0.67s
+mute_hardware
+
+sleep 0.67s
+mute_hardware
 "${work_dir}"/audio-relink-virtual-devices.sh
 
 ### mute mic feedback sink
 pactl set-source-mute "feedback_source" 1
 
-### mute all hardware (laptop/mobile)
-if [ "$(cat /etc/hostname)" = "cookiethinker" ]; then
-	for alsa_dev in $(pactl list sinks short | awk '/alsa/ {print $2}'); do
-		pactl set-sink-mute "${alsa_dev}" 1
-	done
-fi
-
-### mute hdmi outputs on desktop
-if [ "$(cat /etc/hostname)" = "cookieclicker" ]; then
-	for alsa_dev in $(pactl list sinks short | awk '/alsa_output.pci-0000_44_00.1.pro-output/ {print $2}'); do
-		pactl set-sink-mute "${alsa_dev}" 1
-	done
-fi
+sleep 0.42s
+mute_hardware
 
 echo "Audio connection setup finished!"
 
