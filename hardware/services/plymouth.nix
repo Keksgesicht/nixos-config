967536b7e0dba1a0cba6d674821eddf562b55a45
Jan B <git@keksgesicht.net>
2024-10-15 14:55:47 +0200

    plymouth is now fully cookie branded

--- hardware/services/plymouth.nix
+++ hardware/services/plymouth.nix
@@ -1,10 +1,20 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
+let
+  logoFile = ../../files/face.png;
+  themePkg = pkgs.plasma5Packages.breeze-plymouth;
+in
 {
   boot.plymouth = {
     enable = true;
     theme = "breeze";
-    logo = ../../files/face.png;
+    logo = logoFile;
+    themePackages = [ (themePkg.override {
+      inherit logoFile;
+      logoName = "keks";
+      osName = "KexOS";
+      osVersion = config.system.nixos.release;
+    }) ];
   };
 
   # replace all log output with black screen
