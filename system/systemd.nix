cdc040057c2d67a106e38ae53c7b2aebd7231474
Jan B. <git@keksgesicht.net>
2024-10-18 17:39:42 +0200

    fix spelling mistake in journal config

--- system/systemd.nix
+++ system/systemd.nix
@@ -8,19 +8,19 @@
     if (config.networking.hostName == "cookieclicker") then
       ''
       SystemMaxUse=4G
-      SystemdKeepFree=16G
+      SystemKeepFree=16G
       SystemMaxFiles=512
       ''
     else if (config.networking.hostName == "cookiethinker") then
       ''
       SystemMaxUse=2G
-      SystemdKeepFree=8G
+      SystemKeepFree=8G
       SystemMaxFiles=512
       ''
     else
       ''
       SystemMaxUse=1G
-      SystemdKeepFree=4G
+      SystemKeepFree=4G
       SystemMaxFiles=64
       ''
   ;
